a2a421c35948c2c9b75ae89255c435dd373d04b8
Владислав Золотар <zolotar.v@donnu.edu.ua>
2024-12-19 08:53:51 +0200

    polish

--- Program.cs
+++ Program.cs
@@ -4,61 +4,52 @@ namespace Demo_0002.TimeLib
 {
     public class Time
     {
-        private uint seconds;
+        private readonly uint seconds;
 
-        // Конструктор за замовчуванням
         public Time()
         {
             seconds = 0;
         }
 
-        // Конструктор з параметрами
         public Time(int hours, int minutes, int seconds)
         {
             this.seconds = (uint)((hours * 60 + minutes) * 60 + seconds);
         }
 
-        // Властивості для годин, хвилин і секунд
         public int Hours => (int)(seconds / 3600);
         public int Minutes => (int)(seconds / 60 % 60);
         public int Seconds => (int)(seconds % 60);
 
-        // Додавання часу
         public Time AddTime(Time other)
         {
             return new Time(0, 0, (int)(this.seconds + other.seconds));
         }
 
-        // Віднімання часу
         public Time SubtractTime(Time other)
         {
             int resultSeconds = (int)this.seconds - (int)other.seconds;
             if (resultSeconds < 0)
             {
-                resultSeconds = 0; // Час не може бути від'ємним
+                resultSeconds = 0; 
             }
             return new Time(0, 0, resultSeconds);
         }
 
-        // Переведення у секунди
         public uint ToSeconds()
         {
             return seconds;
         }
 
-        // Переведення секунд у час
         public static Time FromSeconds(uint seconds)
         {
             return new Time(0, 0, (int)seconds);
         }
 
-        // Конвертація об'єкта у рядок
         public override string ToString()
         {
             return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
         }
 
-        // Зчитування з файлу
         public static Time FromFile(string filePath)
         {
             var lines = System.IO.File.ReadAllText(filePath).Split(':');
@@ -68,13 +59,11 @@ namespace Demo_0002.TimeLib
             return new Time(hours, minutes, seconds);
         }
 
-        // Запис у файл
         public void ToFile(string filePath)
         {
             System.IO.File.WriteAllText(filePath, ToString());
         }
 
-        // Головна функція для демонстрації
         public static void Main(string[] args)
         {
             Time time1 = new Time(2, 45, 30);
@@ -94,7 +83,6 @@ namespace Demo_0002.TimeLib
             Time fromSeconds = Time.FromSeconds(seconds);
             Console.WriteLine($"Час з секунд: {fromSeconds}");
 
-            // Приклад роботи з файлом
             string filePath = "time.txt";
             time1.ToFile(filePath);
             Time readTime = Time.FromFile(filePath);
